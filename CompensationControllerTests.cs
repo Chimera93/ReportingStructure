0899ad25bf75ea90ea71deaacc892cf5245c70b5
Chris Simmons <css9343@gmail.com>
2023-08-04 09:51:53 -0700

    Add files via upload

---
+++ CompensationControllerTests.cs
@@ -0,0 +1,111 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+using CodeChallenge.Models;
+
+using CodeCodeChallenge.Tests.Integration.Extensions;
+using CodeCodeChallenge.Tests.Integration.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeChallenge.Tests.Integration
+{
+
+    [TestClass]
+    public class CompensationControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        // Attribute ClassInitialize requires this signature
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer();
+            _httpClient = _testServer.NewClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void CreateCompensationByEmployeeID_ExistingEmployee_Returns_Created()
+        {
+            // Arrange
+            var compensation = new Compensation()
+            {
+                employee = new Employee()
+                {
+                    EmployeeId = "03aa1462-ffa9-4978-901b-7c001562cf6f",
+                    Department = "Engineering",
+                    FirstName = "Pete",
+                    LastName = "Best",
+                    Position = "Developer VI"
+                },
+                salary = 1.23,
+                effectiveDate = DateTime.Now                
+            };
+
+            var requestContent = new JsonSerialization().ToJson(compensation);
+
+            // Execute
+            var postRequestTask = _httpClient.PostAsync("api/compensation",
+               new StringContent(requestContent, Encoding.UTF8, "application/json"));
+            var response = postRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+            var newComp = response.DeserializeContent<Compensation>();
+            Assert.IsNotNull(newComp);
+        }
+
+        [TestMethod]
+        public void GetCompensationByEmployeeID_Returns_Ok()
+        {
+            var compensation = new Compensation()
+            {
+                employee = new Employee()
+                {
+                    EmployeeId = "03aa1462-ffa9-4978-901b-7c001562cf6f",
+                    Department = "Engineering",
+                    FirstName = "Pete",
+                    LastName = "Best",
+                    Position = "Developer VI"
+                },
+                salary = 1.23,
+                effectiveDate = DateTime.Now
+            };
+
+            var requestContent = new JsonSerialization().ToJson(compensation);
+
+            // Execute
+            var postRequestTask = _httpClient.PostAsync("api/compensation",
+               new StringContent(requestContent, Encoding.UTF8, "application/json"));
+            var response = postRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+            // Arrange
+            var employeeId = "03aa1462-ffa9-4978-901b-7c001562cf6f";
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/compensation/{employeeId}");
+            response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var comp = response.DeserializeContent<Compensation>();
+            Assert.AreEqual(comp.employee.EmployeeId, employeeId);
+        }
+    }
+}
---
+++ ReportingStructureControllerTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+
+using CodeChallenge.Models;
+
+using CodeCodeChallenge.Tests.Integration.Extensions;
+using CodeCodeChallenge.Tests.Integration.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeChallenge.Tests.Integration
+{ 
+
+    [TestClass]
+    public class ReportingStructureControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        // Attribute ClassInitialize requires this signature
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer();
+            _httpClient = _testServer.NewClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void GetReportingStructure_Returns_NotFound()
+        {
+            // Arrange
+            var employeeID = "Invalid_Id";
+            var requestContent = new JsonSerialization().ToJson(employeeID);
+
+            // Execute
+            var postRequestTask = _httpClient.GetAsync($"api/reportingstructure/{employeeID}");
+            var response = postRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetReportingStructure_Returns_Ok()
+        {
+            // Arrange
+            var employeeID = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var requestContent = new JsonSerialization().ToJson(employeeID);
+
+            // Execute
+            var postRequestTask = _httpClient.GetAsync($"api/reportingstructure/{employeeID}");
+            var response = postRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void GetReportingStructure_Returns_CorrectStructure()
+        {
+            // Arrange
+            var employeeID = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var requestContent = new JsonSerialization().ToJson(employeeID);
+
+            // Execute
+            var postRequestTask = _httpClient.GetAsync($"api/reportingstructure/{employeeID}");
+            var response = postRequestTask.Result;
+
+            // Assert
+            var reportingStructure = response.DeserializeContent<ReportingStructure>();
+
+            Assert.AreEqual(reportingStructure.employee.EmployeeId, employeeID);
+        }
+
+        [TestMethod]
+        public void GetReportingStructure_Returns_CorrectReportsCount()
+        {
+            // Arrange
+            var employeeID = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var expectedReportsCount = 4;
+
+            var requestContent = new JsonSerialization().ToJson(employeeID);
+
+            // Execute
+            var postRequestTask = _httpClient.GetAsync($"api/reportingstructure/{employeeID}");
+            var response = postRequestTask.Result;
+
+            // Assert
+            var reportingStructure = response.DeserializeContent<ReportingStructure>();
+
+            Assert.AreEqual(reportingStructure.numberOfReports, expectedReportsCount);
+        }
+    }
+}
